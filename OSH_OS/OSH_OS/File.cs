fbb963e2e9df59aef482cb24d043e2cb085a38d7
David Khitrik <david.khitrik@gmail.com>
2019-10-14 01:03:29 +0300

    added: content for class file

--- OSH_OS/OSH_OS/File.cs
+++ OSH_OS/OSH_OS/File.cs
@@ -8,5 +8,61 @@ namespace OSH_OS
 {
     class File
     {
+        static string[] fileTypes = { "txt", "mp3", "pdf", "jpg", "mpg" };
+        string name;
+        int fileType;
+        int size;
+        string content;
+        public File(string Name, int FileType, string Content)
+        {
+            this.name = Name;
+            this.fileType = FileType;
+            this.content = Content;
+            size = content.Length;
+        }
+        public File(File ExistingFile)
+        {
+            this.name = ExistingFile.name;
+            this.fileType = ExistingFile.fileType;
+            this.content = ExistingFile.content;
+            size = content.Length;
+        }
+        public void SetName(string Name)
+        {
+            this.name = Name;
+        }
+        public int GetFileType()
+        {
+            return fileType;
+        }
+        public int GetSize()
+        {
+            return size;
+        }
+        public string GetFullName()
+        {
+            return name + "." + fileTypes[fileType];
+        }
+        public override string ToString()
+        {
+            return "Name: " + GetFullName() + ", Size: " + GetSize();
+        }
+        public static bool NonUnique(File[] Files, File NewFile)
+        {
+            for (int i = 0; i < Files.Length; i++)
+            {
+                if (Files[i].GetFullName() == NewFile.GetFullName())
+                    return true;
+            }
+            return false;
+        }
+        public static int GetFileTypesCount()
+        {
+            return fileTypes.Length;
+        }
+        public static string GetFileTypeByIndex(int index)
+        {
+            return fileTypes[index];
+        }
     }
 }
