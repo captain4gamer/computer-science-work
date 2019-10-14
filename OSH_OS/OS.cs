1b45d81f41a8411a270abeb1f31d79cb4b5cbd17
David Khitrik <david.khitrik@gmail.com>
2019-10-14 14:43:58 +0300

    6.0V

--- OSH_OS/OS.cs
+++ OSH_OS/OS.cs
@@ -59,16 +59,97 @@ namespace OSH_OS
                 }
             }
         }
-        public void ListFolderContent()
+        public void ListFolderContent(string Path)
         {
+            Folder folder = FindFolder(Path);
+            folder.ShowContentList();
         }
-        public void ListFolderContentTypes()
+        public void ListFolderContentTypes(string Path)
         {
-
+            Folder folder = FindFolder(Path);
+            folder.ShowFolderContentTypes();
         }
         public void Run()
         {
-
+            Console.WriteLine("1. To create a new folder, type 1 and then press enter");
+            Console.WriteLine("2. To create a new file, type 2 and then press enter");
+            Console.WriteLine("3. To delete a file, type 3 and then press enter");
+            Console.WriteLine("4. To copy a file, type 4 and then press enter");
+            Console.WriteLine("5. To move a file, type 5 and then press enter");
+            Console.WriteLine("6. To delete a folder, type 6 and then press enter");
+            Console.WriteLine("7. To list folder content, type 7 and then press enter");
+            Console.WriteLine("8. To list folder content types, type 8 and then press enter");
+            Console.WriteLine("9. To exit, type 9 and press enter");
+            int func = int.Parse(Console.ReadLine());
+            if (func == 1)
+            {
+                Console.WriteLine("Please enter the name of the new folder");
+                string name = Console.ReadLine();
+                Console.WriteLine("Please enter the path in which the folder will be created");
+                string path = Console.ReadLine();
+                CreateNewFolder(name,path);
+            }
+            if (func == 2)
+            {
+                Console.WriteLine("Please enter the name of the new file");
+                string name = Console.ReadLine();
+                Console.WriteLine("Please enter the type of the file");
+                int FileType = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the content of the new file");
+                string content = Console.ReadLine();
+                Console.WriteLine("Please enter the path in which the file will be created");
+                string path = Console.ReadLine();
+                CreateNewFile(name, path, FileType, content);
+            }
+            if (func == 3)
+            {
+                Console.WriteLine("Please enter the name of the file");
+                string name = Console.ReadLine();
+                Console.WriteLine("Please enter the path of the file");
+                string path = Console.ReadLine();
+                RemoveFile(name,path);
+            }
+            if (func == 4)
+            {
+                Console.WriteLine("Please enter the name of the path of the file");
+                string fullpath = Console.ReadLine();
+                Console.WriteLine("Please enter the destination of the file");
+                string destination = Console.ReadLine();
+                CopyFile(fullpath,destination);
+            }
+            if (func == 5)
+            {
+                Console.WriteLine("Please enter the path of the folder in which the file is found");
+                string Origin = Console.ReadLine();
+                Console.WriteLine("Please enter the destination of the file");
+                string Destination = Console.ReadLine();
+                Console.WriteLine("Please enter the name of the file");
+                string Name = Console.ReadLine();
+                Console.WriteLine("Please enter the path in which the file will be created");
+                MoveFile(Origin,Destination,Name);
+            }
+            if (func == 6)
+            {
+                Console.WriteLine("Please enter the name of the folder");
+                string name = Console.ReadLine();
+                Console.WriteLine("Please enter the path of the folder");
+                string path = Console.ReadLine();
+                RemoveFolder(name,path);
+            }
+            if (func == 7)
+            {
+                Console.WriteLine("Please enter the path of the folder");
+                string path = Console.ReadLine();
+                ListFolderContent(path);
+            }
+            if (func == 8)
+            {
+                Console.WriteLine("Please enter the path of the folder");
+                string path = Console.ReadLine();
+                ListFolderContentTypes(path);
+            }
+            if (func == 9)
+                Environment.Exit(0);
         }
         private Folder FindFolder(string path)
         {
